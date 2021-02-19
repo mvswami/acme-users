6675c36780e1a384cd808954bce68c7fa7633ab6
Manu Swami <swami.manu@gmail.com>
2021-02-18 21:32:41 -0500

    second update

--- src/index.js
+++ src/index.js
@@ -4,15 +4,33 @@ const userList = document.getElementById("user-list");
 const users = new Array(50).fill("").map((_) => {
   return faker.helpers.userCard();
 });
-console.log(faker.helpers.userCard());
 
-const html = users
-  .map((user, index) => {
-    return `<li><a href="#${index}">${user.name}</a></li>`;
-  })
-  .join("");
+let curr = window.location.hash.slice(1) * 1;
+console.log(curr);
 
-userList.innerHTML = html;
+const render = (users) => {
+  console.log(users);
+  const html = `${users
+    .map((user, index) => {
+      return `<li><a href="#${index}">User: ${user.name}</a>
+    ${
+      curr === index
+        ? `<ul>
+    <li>Company: ${user.company.name}</li>
+    <li>Street: ${user.address.street}</li>
+    </ul>`
+        : " "
+    }
+    </li>`;
+    })
+    .join("")}`;
+  userList.innerHTML = html;
+};
+
+window.addEventListener("hashchange", () => {
+  curr = window.location.hash.slice(1) * 1;
+  render(users);
+});
 
 module.exports = {
   users,
